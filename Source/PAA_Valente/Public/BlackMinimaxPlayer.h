9605e983707a6876a3dd1f142bb7670fdcc5399e
noncedikenny <emanuelegmvalente@libero.it>
2024-03-30 19:29:16 +0100

    Inizio Minimax, se faccio casino reverto qui e ciaone

---
+++ Source/PAA_Valente/Public/BlackMinimaxPlayer.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerInterface.h"
+#include "ChessGameInstance.h"
+#include "ChessGameMode.h"
+#include "Piece.h"
+#include "Kismet/GameplayStatics.h"
+#include "BlackMinimaxPlayer.generated.h"
+
+UCLASS()
+class PAA_VALENTE_API ABlackMinimaxPlayer : public APawn, public IPlayerInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABlackMinimaxPlayer();
+	UChessGameInstance* GameInstance;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	bool bIsACapture = false;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void OnTurn() override;
+	virtual void OnWin() override;
+	virtual void OnLose() override;
+	virtual bool IsCheckStatus() override;
+
+	int32 EvaluateGrid(TMap<FVector2D, ATile*>& Board);
+	int32 MiniMax(TMap<FVector2D, ATile*>& Board, int32 Depth, bool IsMax);
+	FVector2D FindBestMove(TMap<FVector2D, ATile*>& Board);
+};
